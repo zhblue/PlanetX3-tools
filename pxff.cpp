8d7f0d006cf185681f11f23073c466dae9d4c02a
张浩斌 <newsclan@gmail.com>
2020-02-21 13:31:29 +0800

    Update pxff.cpp

--- pxff.cpp
+++ pxff.cpp
@@ -1,14 +1,46 @@
+/*written in turbo c 2.0 */
 #include<stdio.h>
 int main(){
    unsigned char d[36884];
-   long int i;
+   long int i,j,base;
    long pos=36867L;
    FILE * fp=fopen("savegame.dat","rb");
    fread(d,1,36884L,fp);
-   
-   d[pos+0]=d[pos+1]=d[pos+2]=255;  // mine gas power = 255
-   
-   for(pos=0x8700L;pos<0x8740L;pos++){  // all unit life = 255
+
+
+   /*  map code dump
+   base=16*9;
+   for(i=0;i<256&&(i+base<256);i++){
+     j=i/16;
+     d[(i%16)+j*256]=i+base;
+
+   }
+   */
+   /* find home  */
+   for(i=0;i<0x8000;i++){
+	if(d[i]==0x7c) {
+		base=i;
+		break;
+	}
+   }
+   /* secure the base  */
+   if(i<0x8000){
+      for(i=-8;i<=8;i++){
+	 for(j=-8;j<=8;j++){
+		if (i*i+j*j>36){
+		    d[base+i*256+j]=0x0f;
+		}
+
+	 }
+
+      }
+
+   }
+
+
+
+   d[pos+0]=d[pos+1]=d[pos+2]=255;
+   for(pos=0x8700L;pos<0x873fL;pos++){
      d[pos]=0xff;
 
    }
